381c54e8c571ae7bb57d5c5ea656451f5be49e48
Ash Kyd <ash@kyd.com.au>
2015-08-05 20:37:42 +1000

    +webaudio & timing/buffering

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-function Sequencer(opts){
-    var _this = this;
-    _this.opts = opts;
-    _this.position = 0;
-    _this.jsfxr  = opts.jsfxr || window.jsfxr;
-    _this.initLoops();
+function base64ToArrayBuffer(base64) {
+    var binary_string = atob(base64.substr(base64.indexOf(',')+1));
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
 }
 
-Sequencer.prototype = {
-    initLoops: function(){
-        var _this = this;
-        var song = _this.opts.song;
-        var loops = _this.opts.loops;
-        _this.opts.song = song.map(function(beat, i){
+function Sequencer(opts){
+    var _this = this;
+    var position = 0;
+    var loop = 0;
+    var ctx = new (window.AudioContext || webkitAudioContext)();
+    _this.buffer = opts.buffer || 0.2;
+
+    var instruments, song, startTime;
+
+    this.initLoops = function(opts){
+        instruments = {};
+        Object.keys(opts.instruments).forEach(function(key){
+            ctx.decodeAudioData(base64ToArrayBuffer(opts.instruments[key]), function(buffer){
+                console.log('Setting', key, buffer);
+                instruments[key] = buffer;
+            });
+        });
+        // init loops
+        song = opts.song;
+        song = song.map(function(beat, i){
             var newBeat = [];
             beat.forEach(function(beat){
-                var loop = _this.opts.loops[beat];
-                if(_this.opts.instruments[beat]){
+                var loop = opts.loops[beat];
+                if(opts.instruments[beat.n || beat]){
                     // return this beat
                     newBeat.push(beat);
                 } else if(loop) {
@@ -25,50 +41,84 @@ Sequencer.prototype = {
                         song[i+j] = song[i+j].concat(loop[j]);
                     }
                 } else {
-                    console.error('Instrument or loop not found', beat);
+                    // console.error('Instrument or loop not found', beat);
                 }
             });
             return newBeat;
         });
-    },
-    play: function(){
+    };
+
+    this.initLoops(opts);
+
+    function playSound(instrument, time){
+        var source = ctx.createBufferSource(); // creates a sound source
+        var buffer = instruments[instrument.n || instrument];
+        source.playbackRate.value = instrument.p || 1;
+        source.buffer = buffer;                // tell the source which sound to play
+        source.connect(ctx.destination);       // connect the source to the context's destination (the speakers)
+
+        var start = time || 0;
+        source.start(start);
+    }
+
+    this.play = function(){
         var _this = this;
         var i;
         var instrument;
         _this.interval = setInterval(function(){
-            var beat = _this.opts.song[_this.position];
-            if(!beat){
-                return _this.stop();
+            if(!startTime){
+                startTime = ctx.currentTime;
             }
 
-            for(i=0; i<beat.length; i++){
-                instrument = _this.opts.instruments[beat[i]];
-                instrument.currentTime = 0;
-                instrument.play();
-            }
+            // Go through the next 100 beats or until we fill the buffer.
+            for(var j=0; j<100; j++){
+                // Pick this one.
+                var beat = song[position];
 
-            if(++_this.position >= _this.opts.song.length){
-                if(_this.opts.loop){
-                    _this.position = 0;
+                // Wrap our loop around/finish it up.
+                if(position >= song.length){
+                    if(opts.loop){
+                        position = 0;
+                        loop++;
+                    } else {
+                        _this.stop();
+                    }
+                    if(opts.onComplete){
+                        opts.onComplete();
+                    }
                 }
-                if(_this.opts.onComplete){
-                    _this.opts.onComplete();
+
+                // The point at which this set of sounds is to be played.
+                // Based on start time, position in song, and number of loops.
+                var playAt = startTime + (opts.loopSpeed*position + song.length*loop*opts.loopSpeed)/1000;
+
+                // If the buffer is more than 1.4 seconds, stop populating it.
+                if(playAt > ctx.currentTime + _this.buffer){
+                    console.log('Buffer full');
+                    break;
                 }
+
+                // Loop through all instruments in this beat & queue them.
+                for(i=0; i<beat.length; i++){
+                    playSound(beat[i], playAt);
+                }
+                position++;
             }
-        },_this.opts.loopSpeed);
+        }, this.buffer*500);
         return _this;
-    },
-    stop: function(){
+    };
+
+    this.stop = function(){
         var _this = this;
         if(_this.interval){
             clearInterval(_this.interval);
             _this.interval = false;
         }
-        _this.position = 0;
+        position = 0;
         return _this;
-    }
-};
+    };
+}
 
 if(typeof module !== 'undefined'){
-    module.exports = sequencer;
+    module.exports = Sequencer;
 }
--- sample.js
+++ sample.js
@@ -2,14 +2,13 @@
 // You can make new instruments with this tool:
 // http://www.superflashbros.net/as3sfxr/
 function makeJSFXRAudio(initArray){
-    var a = new Audio();
-    a.src = jsfxr(initArray);
-    return a;
+    return jsfxr(initArray);
 }
 var instruments = {
     cymbal: makeJSFXRAudio([3,,0.1787,,0.1095,0.502,,,,,,,,0.2868,,,,,1,,,0.1,,0.5]),
     drum: makeJSFXRAudio([3,,0.1787,,0.1095,0.17,,-0.58,,,,,,0.2868,,,,,1,,,0.1,,0.5]),
-    bass: makeJSFXRAudio([0,,0.1897,0.2618,1,0.12,,0.02,,,,,,0.4812,-0.1783,,,,1,,,0.132,,0.5])
+    bass: makeJSFXRAudio([0,,0.1897,0.2618,1,0.12,,0.02,,,,,,0.4812,-0.1783,,,,1,,,0.132,,0.5]),
+    wave: makeJSFXRAudio([0,0,0.037985307862982154,0.5060126532800495,0.4385391124524176,0.5989582167007029,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0.4])
 };
 
 // Milliseconds per beat.
@@ -27,14 +26,14 @@ var loops = {
 
 // The song! Put instruments/loops in here.
 var song = [
-    ['drumloop'],
-    [],
-    [],
-    [],
-    ['drumloop'],
-    [],
-    [],
-    [],
+    ['drumloop', 'wave'],
+    [{n:'wave',p:1.06}],
+    [{n:'wave',p:1.12}],
+    [{n:'wave',p:1.18}],
+    ['drumloop',{n:'wave',p:1.24}],
+    [{n:'wave',p:1.3}],
+    [{n:'wave',p:1.36}],
+    [{n:'wave',p:1.42}],
     ['drumloop', 'bass'],
     [],
     [],
@@ -52,6 +51,7 @@ var s = new Sequencer({
     loops: loops, // Loops
     song: song, // The actual song
     loop: true, // Loop over and over
+    buffer: 1.4, // seconds buffer. ~min Chrome lets us have in a background tab
     onComplete: function(){
         // Call me every time the song ends
         console.log('Song looping');
@@ -62,4 +62,8 @@ var s = new Sequencer({
 window.setTimeout(function(){
     console.log('starting');
     s.play();
-}, 2000);
+}, 1000);
+
+document.querySelector('button').onclick = function(){
+    s.stop();
+};
